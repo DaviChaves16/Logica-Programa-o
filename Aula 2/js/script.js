d2a026ab05a33158831a02d58c2eaf63f675f7ac
DaviChaves16 <124737357+DaviChaves16@users.noreply.github.com>
2023-02-07 18:29:45 -0300

    Add files via upload

---
+++ Aula 2/js/script.js
@@ -0,0 +1,7 @@
+function somar(){
+    const x = Number(document.getElementById("x").value);
+    const y = Number(document.getElementById("y").value);
+    let resultado= x+y;
+    alert(resultado);
+
+}
